4ff4253adf43f86a0c5e4dddd0368daed5c97951
Rockwellwindsor <rockwellwindsor@gmail.com>
2017-02-24 22:24:03 -0600

    Development
    Added redirect if user is not logged in.
    
    Changes to be committed:
        modified:   app/controllers/pages_controller.rb
        modified:   app/views/pages/profile.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
-  def profile
-  end
+
+    before_action :set_user
+
+    def profile
+        if @user
+            redirect_to root_path
+        else
+            redirect_to new_user_session_path
+        end
+    end
+
+    private
+
+    def set_user
+        if user_signed_in?
+            @user = User.find(current_user.id)
+        else
+            @user = nil
+        end
+    end
 end
